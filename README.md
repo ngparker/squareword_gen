79b5b6b77b180f39d3571802b3e7796124cb75eb
Nathan Parker <ngparker@users.noreply.github.com>
2023-10-28 21:51:07 +0000

    Add square classification, and do some cleanup

--- README.md
+++ README.md
@@ -1,16 +1,56 @@
 # squareword_gen
-This is a set of experimenal algorithms to generate valid
-NxN squares of words, where both columns and row are words
-from a given dictionary.
+This is an experiment to try generatating valid NxN squares of words,
+where both columns and row are words from a given dictionary.
 
-It was inspired by squareword.com, and an algorithm from Annalee Flower Horne.
+It was inspired by the squareword.com game, and an algorithm from Annalee Flower Horne.
+
+It outputs things like this:
+
+```
+WordSquare 17 from word 4252/5000: double-word-square, unique
+  gnomes
+  repent
+  outage
+  origin
+  moment
+  snares
+```
+
+## Usage
+
+```
+usage: squareword_gen.py [-h] [--freq_csv_file FREQ_CSV_FILE]
+                         [--scrabble_words_file SCRABBLE_WORDS_FILE]
+                         [--top_n TOP_N] [--word_len WORD_LEN]
+                         [--double_squares_only] [--log_details]
+                         [--just_benchmark]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --freq_csv_file FREQ_CSV_FILE
+                        CSV file where the first row is words, sorted in most
+                        popular first
+  --scrabble_words_file SCRABBLE_WORDS_FILE
+                        Text file of valid words to use
+  --top_n TOP_N         Cutoff for N most popular words to use
+  --word_len WORD_LEN   Len of words to use
+  --double_squares_only
+                        Print only valid double squares
+  --log_details
+  --just_benchmark
+```
 
 ## TODO
-* Get a better word-frequency list. This one seems to lean very heavily towards
-words seen online, rather than in books.
-* Filter out transpose-identical squares,  and squares with dup words.
+
+* Get a better word-frequency list. The one here seems to
+lean very heavily towards words seen online, rather than in
+books. The [Wiktionary:Frequency lists/English/TV and Movie Scripts
+(2006)](https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/English/TV_and_Movie_Scripts_(2006))
+looks good, but I haven't found it in machine-readable
+form. Another possibility is [Leipzig Corpora
+Collection](https://wortschatz.uni-leipzig.de/en/download/).
 
 
 ## Attribution:
  *  `unigram_freq.txt`: from https://www.kaggle.com/datasets/rtatman/english-word-frequency/
- *  `scrabble_words.txt`: is from https://raw.githubusercontent.com/redbo/scrabble/master/dictionary.txt
+ *  `scrabble_words.txt`: is from https://raw.githubusercontent.com/redbo/scrabble/master/dictionary.txt (https://github.com/redbo/scrabble)
--- squareword_gen.py
+++ squareword_gen.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Generate NxN squares of words, like that would be used by https://squareword.org/
-This is an experiment in how to generate them, and what words would show up
+This is an experiment in how to generate them, and to see what words would show up
 most commonly.
 
 Nathan Parker
@@ -8,11 +8,11 @@ Nathan Parker
 
 import argparse
 import csv
-from itertools import product
+import random
+import time
 
 
 LOG_DETAILS = False
-
 
 class Logger:
     """A simple logger class that lets you set a prefix string.
@@ -69,52 +69,55 @@ def GetWorkingWords(freq_csv_file, scrabble_words_file, top_n, word_len):
     return working_words
 
 
-def BuildWordTrie(working_words, word_len):
-    """Build a trie and return it
+class WordTrie(dict):
+  """ Datastructure holding a trie of words all the same length.
+    
+    This is stored as as a dict of single chars, each pointing to a trie,
+    making up valid words.  The last char of a word has a value of "True"
+    rather than another dict.  
+    
+    I tried another implementation that uses 26-element arrays in stead of
+    dicts, but it's 3x slower for some reason (Python's dicts are fast).
+  """
 
-      Args: 
-        working_words: list of words all length word_len
-        word_len: integer
+  def __init__(self, word_list):
+      """
+        Generate and store the trie.
 
-      Returns:
-        The trie, which is a dict of single chars, each pointing to a trie, making up valid words
-        The last char of a word has a value of "True" rather than another dict.
-     """
+        Args:
+          word_list: list of strings N-chars long, used to generate the trie.
+      """
+      for word in word_list:
+          # Start at the root for each word
+          cur_trie = self
 
-    root = dict()
-    for word in working_words:
-        # Start at the root for each word
-        cur_dict = root
-
-        # Create a dict for each node of the trie for all but the last letter
-        for letter in word[:-1]:
-            cur_dict = cur_dict.setdefault(letter, {})
-        # We save some RAM (like 1/2 of it) by not putting an empty dict in
-        # the leaf node. Just put the bool True.
-        cur_dict.setdefault(word[-1], True)
-
-    return root
+          # Create a dict for each node of the trie for all but the last letter
+          # This traverses down the tree.
+          for letter in word[:-1]:
+              cur_trie = cur_trie.setdefault(letter, WordTrie([]))
+          # The last leaf node is a "bool True" rather than an empty WordTrie.
+          cur_trie.setdefault(word[-1], True)
 
 
 def GenWordsFromValidChars(word_trie, valid_next_row_chars):
     """Return a generator that produces valid words.
 
       Args:
-        word_trie: dict-trie of single chars making up valid words
+        word_trie: A node of a WordTrie, root or not.
         valid_next_row_chars: N-elem list of lists, with unique chars allowed at
            that position.
      Returns:
        Generator that produces valid words (strings)
     """
-
     # Iterate over all the chars in position zero, and see if they're
     # in the trie. If so, see if we can make a word.
-    depth = len(valid_next_row_chars)  # this is inverse depth, really
-    # print("  G: -- Called with %d positions left" % depth)
+
+    #depth = len(valid_next_row_chars)  # this is inverse depth, really
+    #print("  G: -- Called with %d positions left" % depth)
     for char_to_try in valid_next_row_chars[0]:
         if char_to_try not in word_trie:
             # No word down this part of the trie
-            # print("  G%d: %s not in trie" % (depth, char_to_try))
+            #print("  G%d: %s not in trie" % (depth, char_to_try))
             continue
 
         this_node = word_trie.get(char_to_try)
@@ -122,11 +125,11 @@ def GenWordsFromValidChars(word_trie, valid_next_row_chars):
         # Check if it's a leaf node (they're == True rather than a dict)
         # Then this is the last char in the word.
         if this_node == True:
-            # print("  G%d:   Yielding EOW %s" % (depth, char_to_try))
+            #print("  G%d:   Yielding EOW %s" % (depth, char_to_try))
             yield char_to_try
             continue
 
-        # print("  G%d: recusing down %s" % (depth, char_to_try))
+        #print("  G%d: recusing down %s" % (depth, char_to_try))
         # Otherwise, recurse. If this call has no items,
         # then there's no valid suffix from here on, so we won't
         # use this char_to_try.
@@ -143,11 +146,12 @@ def GenSquares(word_trie, start_word):
       valid words in their vertical columns.
 
       Args:
-        word_trie: dict-trie of single chars making up valid words
-        start_word: string of one word from the trie
+        word_trie: WordTrie, the root
+        start_word: String of one word from the trie
 
       Returns:
-        list of "squares," where a square is just a list of N words of length N.
+        A generator that yields "squares," where a square is just a list
+        of N words of length N.
      """
 
     # This sets up this new square, and then we call GenSubSquares recursively.
@@ -170,20 +174,20 @@ def GenSquares(word_trie, start_word):
 def GenSubSquares(word_trie, start_word, column_trie_nodes=[], log_prefix=[]):
     """Generate all the partial squares that have start_word at the top, recursively.
 
-      A valid square is a N words of length N that also form
-      valid words in their vertical columns. This is called recursively,
-      by setting coumn_trie_nodes for sub-calls.
+      A valid square is a N words of length N that also form valid words
+      in their vertical columns. This is called recursively, by setting
+      coumn_trie_nodes for sub-calls.
 
       Args:
-        word_trie: dict-trie of single chars making up valid words
+        word_trie: WordTrie, either node or root
         start_word: string of one word from the trie
         columne_trie_nodes: list of N nodes from the word-trie, corresponding to
           each char for the start_word
         log_prefix: Optional list of string to join+prefix output. We'll add start_word to it.
 
       Returns:
-        list of "squares," where a square is just a list of N words of length N.
-        If called recursively, it'll return a list of M words, M<N.
+        Partial squares. It's a generator of list of words (rows) that make
+        up part of the bottom of a square.
     """
 
     # New log obj with start_word added to the prefix.
@@ -191,11 +195,9 @@ def GenSubSquares(word_trie, start_word, column_trie_nodes=[], log_prefix=[]):
     next_log_prefix = log_prefix + [start_word]
     log = Logger('/'.join(next_log_prefix))
 
-    # Called recursively
-    log.log("Sub start word")
+    log.log("Sub square start")
     # Last row?
     if column_trie_nodes[0] == True:
-        log.log("   EOS: END OF SQUARE")
         yield []
         return
 
@@ -209,12 +211,9 @@ def GenSubSquares(word_trie, start_word, column_trie_nodes=[], log_prefix=[]):
     log.log("valid_next_row_chars = %s" % ('-'.join([''.join(charlist) for charlist in
             valid_next_row_chars])))
 
-    # We could enumerate all combinations of next-row-words and
-    # check if they're real words, like...
-    #  for w in product(*valid_next_row_chars):
-    #     print(" word? " + ''.join(w))
-    # But we can short-circuit that.
 
+    # Look at all the valid words using the set of possible chars,
+    # and see if there's a square that could be made from that word.
     for row_word_to_try in GenWordsFromValidChars(word_trie, valid_next_row_chars):
         log.log("  Trying %s" % row_word_to_try)
 
@@ -222,7 +221,7 @@ def GenSubSquares(word_trie, start_word, column_trie_nodes=[], log_prefix=[]):
         next_column_trie_nodes = [column_trie_nodes[i].get(c) for i, c in
                                   enumerate(row_word_to_try)]
 
-        # Recurse
+        # Recurse downward
         for sub_square in GenSubSquares(word_trie, row_word_to_try,
                                         next_column_trie_nodes, next_log_prefix):
             new_sub_square = [row_word_to_try] + sub_square
@@ -230,50 +229,133 @@ def GenSubSquares(word_trie, start_word, column_trie_nodes=[], log_prefix=[]):
             yield new_sub_square
 
 
+def IsDoubleSquare(sq):
+    """ Figure out if this has columns that differ from the rows.
+    """
+
+    for i, row_word in enumerate(sq):
+        col_word = "".join([word[i] for word in sq])
+        if col_word != row_word:
+            return True
+
+    return False
+
+
+def WordsAreUnique(sq, is_double_square):
+    """Figure out if there are any shared words in rows + cols.
+    
+    If not is_double_square, this'll look at just rows.
+    """ 
+
+    words = set()
+    for i, row_word in enumerate(sq):
+        if row_word in words:
+            return False
+        words.add(row_word)
+
+        if is_double_square:
+            col_word = "".join([word[i] for word in sq])
+            if col_word in words:
+                return False
+            words.add(col_word)
+
+    return True
+
+
+def DoSomeBenchmarking(working_words, word_trie):
+    """Print out some benchmarks at how fast we can make squares
+
+      Args:
+        working_words: list of words to use
+        word_trie: root node of WordTrie
+    """
+
+    random.seed(123)
+    random.shuffle(working_words)
+
+    start_time = time.time()
+    squares = 0
+    pos = 0
+    target_time = start_time + 10  # 10 seconds
+    while time.time() < target_time:
+        for sq in GenSquares(word_trie, working_words[pos]):
+          squares += 1
+
+        pos += 1
+        if pos >= len(working_words):
+          pos = 0
+   
+    dur = time.time() - start_time
+    print("Ran %d squares in %.1f sec, or %.0f squares/sec" % (squares, dur,
+          squares / dur))
+
 
 def main():
     """ Main
     """
 
     parser = argparse.ArgumentParser()
+    # Inputs
     parser.add_argument('--freq_csv_file', type=str,
-                        default='unigram_freq.csv')
+                        default='unigram_freq.csv',
+                        help="CSV file where the first row is words, "
+                        "sorted in most popular first")
     parser.add_argument('--scrabble_words_file', type=str,
-                        default='scrabble_words.txt')
+                        default='scrabble_words.txt',
+                        help="Text file of valid words to use")
 
-    parser.add_argument('--top_n', type=int, default=5000)
-    parser.add_argument('--word_len', type=int, default=5)
+    # Knobs
+    parser.add_argument('--top_n', type=int, default=5000,
+                        help="Cutoff for N most popular words to use")
+    parser.add_argument('--word_len', type=int, default=5, help="Len of words to use")
+    parser.add_argument('--double_squares_only', action="store_true",
+                        help="Print only valid double squares")
+
+    # Debugging options
     parser.add_argument('--log_details', action="store_true")
+    parser.add_argument('--just_benchmark', action="store_true")
 
     args = parser.parse_args()
-
-    global LOG_DETAILS
     LOG_DETAILS = args.log_details
-    print("LOG_DETAILS = %d" % LOG_DETAILS)
 
     working_words = GetWorkingWords(args.freq_csv_file, args.scrabble_words_file,
                                     args.top_n, args.word_len)
 
-    word_trie = BuildWordTrie(working_words, args.word_len)
+    word_trie = WordTrie(working_words)
+
+    if args.just_benchmark:
+      DoSomeBenchmarking(working_words, word_trie)
+      return
+
+    # Example valid 6x6 square:
+    #  market
+    #  avenue
+    #  relics
+    #  knight
+    #  euchre
+    #  rested
+
     print("Generating Squares..\n")
-
-    # Example valid square:
-    #  snuff
-    #  airer
-    #  reata
-    #  octet
-    #  seeds
-    # working_words = ["snuff"]
-
-    squares = []
-    for start_word in working_words:
+    sq_num = 0
+    for word_num, start_word in enumerate(working_words):
         for sq in GenSquares(word_trie, start_word):
+            # Collect some classifications of this sq.
+            is_double_square = IsDoubleSquare(sq)
+            words_are_unique = WordsAreUnique(sq, is_double_square)
+            if args.double_squares_only and (not is_double_square
+                    or not words_are_unique):
+                continue
+            desc = "WordSquare %d from word %d/%d: %s-word-square, %s" % (
+                sq_num, word_num, len(working_words),
+                "double" if is_double_square else "single",
+                "unique" if words_are_unique else "non-unique")
+            print(desc)
             for word in sq:
-                print(word)
+                print("  " + word)
             print("")
-            squares.append(sq)
+            sq_num += 1
 
-    print("Made %d squares" % len(squares))
+    print("Made %d squares" % sq_num)
 
 
 if __name__ == "__main__":
